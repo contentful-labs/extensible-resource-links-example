72d8dd5e334f1f2cf9cd66c1475a755b9a8435f0
Cyberxon <marouenbs93@gmail.com>
2024-03-27 11:36:06 +0100

    feat: add form control for instance parameters

--- src/locations/ConfigScreen.tsx
+++ src/locations/ConfigScreen.tsx
@@ -1,68 +1,144 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { ConfigAppSDK } from '@contentful/app-sdk';
-import { Heading, Form, Paragraph, Flex } from '@contentful/f36-components';
-import { css } from 'emotion';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import { Heading, Form, Flex, TextInput, FormControl } from '@contentful/f36-components';
+import { useSDK } from '@contentful/react-apps-toolkit';
 
-export interface AppInstallationParameters {}
+interface AppInstallationParameters {
+  apiEndpoint?: string;
+  storefrontAccessToken?: string;
+  secretExternalResourceProvider?: string;
+  resourceTypes?: string;
+  providerName?: string;
+}
 
 const ConfigScreen = () => {
   const [parameters, setParameters] = useState<AppInstallationParameters>({});
   const sdk = useSDK<ConfigAppSDK>();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
 
   const onConfigure = useCallback(async () => {
-    // This method will be called when a user clicks on "Install"
-    // or "Save" in the configuration screen.
-    // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
-
-    // Get current the state of EditorInterface and other entities
-    // related to this app installation
     const currentState = await sdk.app.getCurrentState();
-
     return {
-      // Parameters to be persisted as the app configuration.
       parameters,
-      // In case you don't want to submit any update to app
-      // locations, you can just pass the currentState as is
       targetState: currentState,
     };
   }, [parameters, sdk]);
 
+  function updateParameters<T extends keyof AppInstallationParameters>(parameterName: T)  {
+    return (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      setParameters({ ...parameters, [parameterName]: value });
+    }
+  }
+
   useEffect(() => {
-    // `onConfigure` allows to configure a callback to be
-    // invoked when a user attempts to install the app or update
-    // its configuration.
-    sdk.app.onConfigure(() => onConfigure());
+    sdk.app.onConfigure(onConfigure);
   }, [sdk, onConfigure]);
 
   useEffect(() => {
     (async () => {
-      // Get current parameters of the app.
-      // If the app is not installed yet, `parameters` will be `null`.
       const currentParameters: AppInstallationParameters | null = await sdk.app.getParameters();
 
       if (currentParameters) {
         setParameters(currentParameters);
       }
 
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
       sdk.app.setReady();
     })();
   }, [sdk]);
 
   return (
-    <Flex flexDirection="column" className={css({ margin: '80px', maxWidth: '800px' })}>
-      <Form>
+      <Flex flexDirection="column" margin="spacingL">
         <Heading>App Config</Heading>
-        <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
-      </Form>
-    </Flex>
+        <Form>
+          <FormControl isRequired isInvalid={!parameters.apiEndpoint}>
+            <FormControl.Label>API endpoint</FormControl.Label>
+            <TextInput
+                value={parameters.apiEndpoint}
+                type="url"
+                name="apiEndpoint"
+                onChange={updateParameters("apiEndpoint")}
+            />
+            <FormControl.HelpText>
+              Provide the url to the API endpoint of the shop
+            </FormControl.HelpText>
+            {!parameters.apiEndpoint && (
+                <FormControl.ValidationMessage>
+                  Please, provide API endpoint
+                </FormControl.ValidationMessage>
+            )}
+          </FormControl>
+
+          <FormControl isRequired isInvalid={!parameters.storefrontAccessToken}>
+            <FormControl.Label>API token</FormControl.Label>
+            <TextInput
+                value={parameters.storefrontAccessToken}
+                name="storefrontAccessToken"
+                onChange={updateParameters("storefrontAccessToken")}
+            />
+            <FormControl.HelpText>
+              Provide the access token of the shop
+            </FormControl.HelpText>
+            {!parameters.storefrontAccessToken && (
+                <FormControl.ValidationMessage>
+                  Please, provide valid API token
+                </FormControl.ValidationMessage>
+            )}
+          </FormControl>
+          <FormControl isRequired isInvalid={!parameters.secretExternalResourceProvider}>
+            <FormControl.Label>Secret External Resource Provider</FormControl.Label>
+            <TextInput
+                value={parameters.secretExternalResourceProvider}
+                type="url"
+                name="apiEndpoint"
+                onChange={updateParameters("secretExternalResourceProvider")}
+            />
+            <FormControl.HelpText>
+              Provide the url to the API endpoint of the shop
+            </FormControl.HelpText>
+            {!parameters.secretExternalResourceProvider && (
+                <FormControl.ValidationMessage>
+                  Please, provide API endpoint
+                </FormControl.ValidationMessage>
+            )}
+          </FormControl>
+
+          <FormControl isRequired isInvalid={!parameters.resourceTypes}>
+            <FormControl.Label>Resource Types</FormControl.Label>
+            <TextInput
+                value={parameters.resourceTypes}
+                type="url"
+                name="apiEndpoint"
+                onChange={updateParameters("resourceTypes")}
+            />
+            <FormControl.HelpText>
+              Provide the url to the API endpoint of the shop
+            </FormControl.HelpText>
+            {!parameters.resourceTypes && (
+                <FormControl.ValidationMessage>
+                  Please, provide API endpoint
+                </FormControl.ValidationMessage>
+            )}
+          </FormControl>
+
+          <FormControl isRequired isInvalid={!parameters.providerName}>
+            <FormControl.Label>Provider Name</FormControl.Label>
+            <TextInput
+                value={parameters.providerName}
+                type="url"
+                name="apiEndpoint"
+                onChange={updateParameters("providerName")}
+            />
+            <FormControl.HelpText>
+              Provide the url to the API endpoint of the shop
+            </FormControl.HelpText>
+            {!parameters.providerName && (
+                <FormControl.ValidationMessage>
+                  Please, provide API endpoint
+                </FormControl.ValidationMessage>
+            )}
+          </FormControl>
+        </Form>
+      </Flex>
   );
 };
 
