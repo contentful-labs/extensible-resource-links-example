568c9bb1c343bec9267bff8b248a723213aadacf
Cyberxon <marouenbs93@gmail.com>
2024-03-27 14:34:56 +0100

    test: one item

--- functions/TMDBResources.ts
+++ functions/TMDBResources.ts
@@ -11,13 +11,25 @@ import {
 type AppInstallationParameters = {
   accessToken: string
 }
-
 const searchHandler = (event: ResourcesSearchRequest, context: FunctionEventContext<AppInstallationParameters>): ResourcesSearchResponse => {
-  return {items: [], pages: {}}
+  return {items: [{id: 'efcwret', title: 'Dune', overview: 'A great movie'}], pages: {}}
 };
 
 const lookupHandler = (event: ResourcesLookupRequest, context: FunctionEventContext<AppInstallationParameters>): ResourcesLookupResponse => {
-  return {items: [], pages: {}}
+  let items
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     Authorization: 'Bearer ',
+  //     accept: 'application/json'
+  //   }
+  // };
+  //
+  // fetch('https://api.themoviedb.org/3/movie/popular?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+  //     .then(response => response.json())
+  //     .then(response => console.log(response))
+  //     .catch(err => console.error(err));
+  return {items: [{title: 'Dune'}], pages: {}}
 };
 
 // @ts-expect-error ...
